d4b813c1efe9c78be7d8cec5b20b653fbc113c3b
Victor Dragnea <dragneavictor@gmail.com>
2017-04-03 23:27:21 +0300

    acasa

--- quote.php
+++ quote.php
@@ -1,4 +1,5 @@
 <?php
+/*
 	$handle = @fopen("http://download.finance.yahoo.com/d/quotes.csv?s={$_GET['symbol']}&f=e1l1", "r");
 	if ($handle !== FALSE) {
 		$data = fgetcsv($handle);
@@ -6,5 +7,25 @@
 			print($data[1]);
 		fclose($handle);
 	}
+*/
+
+	//http://download.finance.yahoo.com/d/quotes.csv?s=FB&f=a
+
+$s = urlencode($_GET['search']);
+$url = 'http://download.finance.yahoo.com/d/quotes.csv?s=$s&f=a';
+$handle = fopen($url, 'r');
+$row = fgetcsv($handle);
+var_dump($row);
+fclose($handle);
+
 ?>
 
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bla</title>
+</head>
+<body>
+	The current price of <?= htmlspecialchars($_GET['search'])?> is $ <?= $row[1] ?>. 
+</body>
+</html>
