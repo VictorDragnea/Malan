bc97e4151efcdc3d6a88c74a66fa146cf7300b9a
Victor Dragnea <dragneavictor@gmail.com>
2017-04-05 14:45:41 +0300

    munca

--- quote.php
+++ quote.php
@@ -10,9 +10,21 @@
 */
 
 	//http://download.finance.yahoo.com/d/quotes.csv?s=FB&f=a
+	//
 
 $s = urlencode($_GET['search']);
-$url = 'http://download.finance.yahoo.com/d/quotes.csv?s=$s&f=a';
+$url = "http://download.finance.yahoo.com/d/quotes.csv?s={$s}&f=a";
+
+$cSession = curl_init();
+curl_setopt($cSession,CURLOPT_URL,$url);
+curl_setopt($cSession, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($cSession, CURLOPT_HEADER, false);
+$result=curl_exec($cSession);
+curl_close($cSession);
+echo $result;
+
+
+
 $handle = fopen($url, 'r');
 $row = fgetcsv($handle);
 var_dump($row);
@@ -26,6 +38,6 @@ fclose($handle);
 	<title>Bla</title>
 </head>
 <body>
-	The current price of <?= htmlspecialchars($_GET['search'])?> is $ <?= $row[1] ?>. 
+	The current price of <?= htmlspecialchars($_GET['search'])?> is $ <?= $row[0] ?>. 
 </body>
 </html>
